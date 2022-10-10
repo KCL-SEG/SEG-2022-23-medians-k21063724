f20872aee966368268f3712366c7012d489f1ee9
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-10 21:40:34 +0100

    Solution to median problem

--- median.py
+++ median.py
@@ -1,12 +1,37 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from turtle import position
+
+
+def CalculateMedian(list_of_numbers):
+    list_of_numbers.sort()
+    if len(list_of_numbers) % 2 == 1:
+        mid_position_index = ( len(list_of_numbers) + 1)/2
+        return list_of_numbers[int(mid_position_index) - 1]
+    else:
+        position = len(list_of_numbers)/2
+        index1 = int(position - 1)
+        index2 = int(index1 + 1)
+
+        return (list_of_numbers[index1] + list_of_numbers[index2]) / 2
+
+
+    
+ 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        input_from_user = input()
+        input_from_user = input_from_user.split(',')
+
+        numbers = [ int(value) for value in input_from_user]
+        numbers.sort()
+        print(numbers)
+        print(CalculateMedian(numbers))
+
     except ValueError:
-        print("Some input could not be converted to a number!")
+        print("Some input coul2,d not be converted to a number!")
     else:
         break
-print(numbers)
+
